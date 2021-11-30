00f336c3e7e03dfb32913d74034bb0ce880aaffc
Cory Thompson <cory.thompson98@gmail.com>
2021-11-30 15:52:07 -0600

    Create questions.js

---
+++ assets/questions.js
@@ -0,0 +1,11 @@
+var myQuestions = [
+    {
+        question: "What are the Javascript Data types?"
+        answers: {
+            a: 'Number, String, Object'
+            b: 'Number, String, Boolean, Object, Undefined'
+            c: 'Number, Boolean, Object, Undefined'
+            d: 'Boolean, String, Object, Undefined'
+        }
+    }
+]
