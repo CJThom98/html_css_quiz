923a959804c38a119abec7d3a1a6cec07a34a925
Cory Thompson <cory.thompson98@gmail.com>
2021-11-30 15:59:55 -0600

    Update questions.js

--- assets/questions.js
+++ assets/questions.js
@@ -6,6 +6,18 @@ var myQuestions = [
             b: 'Number, String, Boolean, Object, Undefined',
             c: 'Number, Boolean, Object, Undefined',
             d: 'Boolean, String, Object, Undefined'
-        }
+        },
+        correctAnswer: 'b'
+    },
+
+    {
+        question: "What are undeclared variables?",
+        answers: {
+            a: 'Variables that do not exist in the program and have not been declared.',
+            b: 'Variables that do no exist in the program.',
+            c: 'Variables that have not been declared.',
+            d: 'Variables that are not working.'
+        },
+        correctAnswer: 'a'
     },
 ]
