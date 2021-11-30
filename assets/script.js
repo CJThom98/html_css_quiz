a643752c42ba30f39cab7d0f571fea57679dfcfa
Cory Thompson <cory.thompson98@gmail.com>
2021-11-30 15:17:56 -0600

    Update script.js

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var scoreDiv = document.querySelector("#scores");
+var gameTimerEl = document.querySelector("#gameTimer");
+var mainEl = document.querySelector("#details");
+
+var test = false;
+var score = 0;
+var quiz = {};
+var quizType = "";
+
