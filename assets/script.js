970fb880d95d5ee44b0cbffd23d41adb7bc17649
Cory Thompson <cory.thompson98@gmail.com>
2021-11-30 16:34:03 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,68 @@
-var scoreDiv = document.querySelector("#scores");
-var gameTimerEl = document.querySelector("#gameTimer");
-var mainEl = document.querySelector("#details");
+var myQuestions = [
+    {
+        question: "This ",
+        answers: {
+            a: 'Number, String, Object',
+            b: 'Number, String, Boolean, Object, Undefined',
+            c: 'Number, Boolean, Object, Undefined',
+            d: 'Boolean, String, Object, Undefined'
+        },
+        correctAnswer: 'b'
+    },
 
-var test = false;
-var score = 0;
-var quiz = {};
-var quizType = "";
+    {
+        question: "What are undeclared variables?",
+        answers: {
+            a: 'Variables that do not exist in the program and have not been declared.',
+            b: 'Variables that do no exist in the program.',
+            c: 'Variables that have not been declared.',
+            d: 'Variables that are not working.'
+        },
+        correctAnswer: 'a'
+    },
 
+    {
+        question: "What's a global variable?",
+        answers: {
+            a: 'Variables used in international',
+            b: 'Variables that are easy to translate',
+            c: 'Variables used throughout your JS code',
+            d: 'Variables that all files of your webpage uses'
+        },
+        correctAnswer: 'c'
+    },
+
+    {
+        question: "If I wanted the browser to ask a question for the user to answer, what would I use?",
+        answers: {
+            a: 'Alert',
+            b: 'Prompt',
+            c: 'Question',
+            d: 'Confirm'
+        },
+        correctAnswer: 'b'
+    },
+
+    {
+        question: "What are the two methods to bring up devtools in Google Chrome?",
+        answers: {
+            a: 'Ctrl+Alt+Delete & F11',
+            b: 'Alt+F4 & F12',
+            c: 'Right click > View Page Source & F1',
+            d: 'Right click > Inspect & F12'
+        },
+        correctAnswer: 'd'
+    },
+];
+
+var nerdQuestions = [
+    {
+        question: "First called a piece of junk, this ship is used by a famous smuggler and his hairy friend.",
+        correctAnswer: "What is The Millennium Falcon?"
+    },
+
+    {
+        question: "It must flow and whoever controls it, controls the universe.",
+        correctAnswer: "What is spice?",
+    }
+]
