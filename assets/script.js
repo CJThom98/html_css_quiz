52f67d9884cedd42b30c63c7ef848ff03722b6e0
Cory Thompson <cory.thompson98@gmail.com>
2021-12-01 14:20:11 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,15 @@ class TriviaGameShow {
 
         this.boardElement = element.querySelector(".board");
         this.scoreCountElement = element.querySelector(".score-count");
-        
+        this.formElement = element.querySelector("form");
+        this.inputElement = element.querySelector("input[name=user-answer]");
+        this.modalElement = element.querySelector(".card-modal");
+        this.clueTextElement = element.querySelector(".clue-text");
+        this.answerElement = element.querySelector(".answer");
+        this.answerTextElement = element.querySelector(".answer_correct-answer-text");
+        this.successTextElement = element.querySelector(".result_answer");
+        this.failTextElement = element.querySelector(".result_fail");
     }
+
+    
 }
