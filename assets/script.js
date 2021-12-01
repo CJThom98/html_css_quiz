a49c0b92e36d17e6c2c5274ef1d355ab1703ba5e
Cory Thompson <cory.thompson98@gmail.com>
2021-12-01 14:14:38 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
 class TriviaGameShow {
-    constructor
+    constructor(element, options={}) {
+
+        this.categories = [];
+        this.clues = {};
+
+        this.currentClue = null;
+        this.score = 0; 
+
+        this.boardElement = element.querySelector(".board");
+        this.scoreCountElement = element.querySelector(".score-count");
+        
+    }
 }
