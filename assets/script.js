85642c25fcffdc836dff7814112d935f293569ec
Cory Thompson <cory.thompson98@gmail.com>
2021-12-02 12:06:01 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,11 @@
-class TriviaGameShow {
-    constructor(element, options={}) {
-
-        this.categories = [];
-        this.clues = {};
-
-        this.currentClue = null;
-        this.score = 0; 
-
-        this.boardElement = element.querySelector(".board");
-        this.scoreCountElement = element.querySelector(".score-count");
-        this.formElement = element.querySelector("form");
-        this.inputElement = element.querySelector("input[name=user-answer]");
-        this.modalElement = element.querySelector(".card-modal");
-        this.clueTextElement = element.querySelector(".clue-text");
-        this.answerElement = element.querySelector(".answer");
-        this.answerTextElement = element.querySelector(".answer_correct-answer-text");
-        this.successTextElement = element.querySelector(".result_answer");
-        this.failTextElement = element.querySelector(".result_fail");
-    }
-
-    
-}
-
 $(window).load(function () {
     $(".trigger_popup_fricc").click(function(){
-        $('.hover_bkgr_fricc').show();
+       $('.hover_bkgr_fricc').show();
     });
     $('.hover_bkgr_fricc').click(function(){
-        $('hover_bkgr_fricc').hide();
+        $('.hover_bkgr_fricc').hide();
     });
-})
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+});
